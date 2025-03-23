fd8e64ec2345b77a034440d29503c2dc329eed23
XuanTruc1109 <132972323+XuanTruc1109@users.noreply.github.com>
2025-03-23 15:28:02 +0700

    Tạo lại component Posts

---
+++ src/app/pages/ui-components/Posts/add-posts/add-posts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-posts',
+  imports: [],
+  templateUrl: './add-posts.component.html',
+  styleUrl: './add-posts.component.scss'
+})
+export class AddPostsComponent {
+
+}
