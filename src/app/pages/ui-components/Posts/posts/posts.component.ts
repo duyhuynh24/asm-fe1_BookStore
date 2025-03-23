13e7d63593fe7dc38bb3a886fcfbf66eae8b56e8
XuanTruc1109 <132972323+XuanTruc1109@users.noreply.github.com>
2025-03-23 22:58:55 +0700

    quản lý bài viết

--- src/app/pages/ui-components/Posts/posts/posts.component.ts
+++ src/app/pages/ui-components/Posts/posts/posts.component.ts
@@ -1,57 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-posts',
-  standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatCardModule
-  ],
+  standalone: true,  // Nếu component này standalone
+  imports: [CommonModule, ReactiveFormsModule], // Thêm ReactiveFormsModule
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.scss'
+  styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent {
-  public postForm: FormGroup; // 🟢 Đảm bảo là `public`
+  public postForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.postForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(5)]],
-      content: ['', [Validators.required, Validators.minLength(20)]],
+      title: [''],
+      content: [''],
       image: [''],
-      category: ['', Validators.required],
+      category: ['']
     });
   }
 
-  get titleControl() {
-    return this.postForm.controls['title'];
-  }
-
-  get contentControl() {
-    return this.postForm.controls['content'];
-  }
-
-  get categoryControl() {
-    return this.postForm.controls['category'];
-  }
-
   onSubmit() {
-    if (this.postForm.valid) {
-      console.log('Bài viết:', this.postForm.value);
-      alert('Bài viết đã được gửi!');
-      this.postForm.reset();
-    }
+    console.log('Bài viết:', this.postForm.value);
+    alert('Bài viết đã được gửi!');
+    this.postForm.reset();
   }
 }
